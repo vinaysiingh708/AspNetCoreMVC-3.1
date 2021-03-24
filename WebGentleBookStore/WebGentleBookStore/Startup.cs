d997b27e8c845b2e874017736fff922538ebf122
vinay singh <vinaysingh708@yahoo.com>
2021-03-24 12:40:33 +0530

    Use static files

--- WebGentleBookStore/WebGentleBookStore/Startup.cs
+++ WebGentleBookStore/WebGentleBookStore/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,7 +29,15 @@ namespace WebGentleBookStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+                RequestPath= "/MyStaticFiles"
+            });
+
+            app.UseRouting();   
 
             app.UseEndpoints(endpoints =>
             {
